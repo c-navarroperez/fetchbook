c67dc7126daea7a0510e4f5ad133552d7761afc4
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-01-08 10:44:28 +0000

    Changed bookSearch API to googleAPI, NYT bestseller API implemented fully. All appears to be working

--- script/script.js
+++ script/script.js
@@ -2,72 +2,65 @@
 const search = document.querySelector("#searchInput");
 const searchBtn = document.querySelector("#searchBtn");
 const main = document.querySelector("main");
+const nytSection = document.querySelector("#nytSection");
 const nytAPIKey = "JGBNorym4yKMbGSVrRthJlg207eHEfsV";
 const booksRunKey = "durnemjx4rdbc0m7r6ma";
-//NYT link for reviews if needed
-// `https://api.nytimes.com/svc/books/v3/reviews.json?title=${title}&api-key=${nytAPIKey}`
 
-// getPrice function, uses ISBN to find average price of book. Works BUT, API price data is lacking
-function getPrice(title) {
-  const proxyURL = "https://api.allorigins.win/get?url=";
-  const apiURL = encodeURIComponent(
-    `https://booksrun.com/api/price/sell/${title}?key=${booksRunKey}`
-  );
-  fetch(proxyURL + apiURL)
-    .then((res) => res.json())
-    .then((data) => {
-      return JSON.parse(data.contents);
-    })
-    .then((data) => {
-      if (data.result.status === "success") {
-        console.log(data.result.text?.Average);
-      } else {
-        throw new Error("Unable to find book in prices database");
-      }
-    })
-    .catch((err) => console.log(err));
+//alt imgLink, if issues arise with googleapis
+// let imgLink = `https://covers.openlibrary.org/b/olid/${coverKey}-S.jpg`;
+
+//toCorrectCase Function, Takes string, converts to Lower case, makes first character uppercase and returns
+function toCorrectCase(str) {
+  let string = [...str.toLowerCase()];
+  let firstLetter = string.splice(0, 1).toString().toUpperCase();
+  let newString = firstLetter + string.join("").toString();
+  return newString;
 }
 
-//parseBooksArray function, after being given an array of data it for loops through it to scrape relevant data. It then uses data to make a fetch request for an image and then uses data to insert HTML. (Future refactor/break into seperate functions?)
+//Google API version
+//parseBooksArray function, after being given an array of data it for loops through it to scrape relevant data and then uses the data to insert to DOM.
 function parseBooksArray(arr) {
+  main.innerHTML = "";
+  //below classlist included for testing purposes
+  nytSection.classList.add("hide");
   for (let i = 0; i < 10; i++) {
-    let title = arr[i].title;
-    let author = arr[i].author_name[0];
-    let publishYear = arr[i].first_publish_year;
-    let isbn = arr[i].isbn[0];
-    let editionCount = arr[i].edition_count;
-    let coverKey = arr[i].cover_edition_key;
-    getPrice(isbn);
-    //alt imgLink, if issues arise with googleapis
-    // let imgLink = `https://covers.openlibrary.org/b/olid/${coverKey}-S.jpg`;
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=title:${title}`)
-      .then((res) => res.json())
-      .then((data) => {
-        return data.items[0].volumeInfo.imageLinks.smallThumbnail;
-      })
-      .then((link) => {
-        main.insertAdjacentHTML(
-          "beforeend",
-          `<div class="bookCard"><h3>${title}</h3><p>${author}</p><p>${publishYear}</p><p>${isbn}</p><p>${editionCount}</p> <img src="${link}" alt="Book Cover">
-          `
-        );
-      });
+    let title = arr[i].volumeInfo.title;
+    let author = arr[i].volumeInfo.authors.join(", ") || "No author found";
+    let publishDate =
+      arr[i].volumeInfo.publishedDate?.slice(0, 4) || "No publish date found";
+    let isbn;
+    if (arr[i].volumeInfo.industryIdentifiers) {
+      isbn = arr[i].volumeInfo.industryIdentifiers[0].identifier;
+    } else {
+      isbn = "No ISBN found";
+    }
+    let description =
+      arr[i].volumeInfo.description || "No description available";
+    let coverImage = arr[i].volumeInfo?.imageLinks?.smallThumbnail;
+    let coverImageHTML = `<img src="${coverImage}" alt="Book Cover">`;
+    if (coverImage === undefined) {
+      coverImageHTML = "<p>No image available</p>";
+    }
+    main.insertAdjacentHTML(
+      "beforeend",
+      `<div class="bookCard item"><h3>Title: ${title}</h3><p>Author: ${author}</p><p>Publish Year: ${publishDate}</p><p>ISBN: ${isbn}</p><p>Description: ${description}</p> ${coverImageHTML}
+      `
+    );
   }
 }
 
 //bookSearch function, searches for book in openLibrary api using given book title
 function bookSearch(title) {
   search.value = "";
-  fetch(`http://openlibrary.org/search.json?q=${title}`)
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=title:${title}`)
     .then((res) => res.json())
     .then((data) => {
-      if (data.numFound === 0) {
+      if (data.totalItems === 0) {
         throw new Error("No results found, please try searching again");
       }
-      let dataArray = data.docs.slice();
+      let dataArray = data.items.slice();
       parseBooksArray(dataArray);
-    })
-    .catch((err) => console.log(err));
+    });
 }
 
 //bestSellers function, utilises NYTimes best seller api to fetch data on page load(not yet)
@@ -78,10 +71,25 @@ function bestSellers() {
     .then((res) => res.json())
     .then((data) => data.results.lists)
     .then((list) => {
-      console.log(list);
+      list.forEach((li) => {
+        let title = li.books[0].title;
+        let author = li.books[0].author;
+        let image = li.books[0].book_image;
+        let description = li.books[0].description;
+        if (description === "") {
+          description = "N/A";
+        }
+        let amazonLink = li.books[0].amazon_product_url;
+        nytSection.insertAdjacentHTML(
+          "beforeend",
+          `<div class="item"><h2>${li.list_name}</h2><p>Title: ${toCorrectCase(
+            title
+          )}</p><p>Author: ${author}</p><p>Description: ${description}</p><a href="${amazonLink}" target="_blank">
+          <img src="${image}" alt="Best Seller image"></a></div>`
+        );
+      });
     });
 }
-bestSellers();
 
 //searchInput addEventListener
 search.addEventListener("keydown", function (e) {
@@ -99,3 +107,6 @@ searchBtn.addEventListener("click", function (e) {
   let input = searchInput.value;
   bookSearch(input);
 });
+
+//bestSellers called on page load to load/fetch NYT section
+bestSellers();
